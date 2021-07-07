438c871c0b4a56f158c2c3d43288d66388a417fc
PureGero <puregero@gmail.com>
2021-07-07 23:51:33 +1000

    Use __dirname to find files

--- http_server.js
+++ http_server.js
@@ -17,7 +17,7 @@ app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use(express.static(path.join('web-app', 'dist')));
+app.use(express.static(path.join(__dirname, 'web-app', 'dist')));
 
 let server;
 const donePromise = new Promise(resolve => {
